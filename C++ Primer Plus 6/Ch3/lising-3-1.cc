1a38860b2c0f562d9dfc0a688510b319a835709f
Ming Li <leemingee1995@gmail.com>
2019-03-04 12:37:44 -0500

    merge

---
+++ C++ Primer Plus 6/Ch3/lising-3-1.cc
@@ -0,0 +1,22 @@
+// Description:
+// 
+
+// Created by Ming Li at 03/03/2019, Sunday
+// Contact: ming.li2@columbia.edu
+// Linkedin: www.linkedin.com/in/leemingee 
+
+#include <iostream>
+#include <climits>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    int n_int = INT_MAX;
+    short n_short = SHRT_MAX;
+    long n_long = LONG_MAX;
+    long long n_llong = LONG_LONG_MAX;
+
+    
+}
