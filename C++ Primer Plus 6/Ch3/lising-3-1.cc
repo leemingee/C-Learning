fb89ac9d84088f0b8c11b85ec06b94a99cba9f42
Ming Li <leemingee1995@gmail.com>
2019-03-04 15:46:14 -0500

    merge done

--- C++ Primer Plus 6/Ch3/lising-3-1.cc
+++ C++ Primer Plus 6/Ch3/lising-3-1.cc
@@ -10,13 +10,25 @@
 using std::cout;
 using std::cin;
 using std::endl;
+using namespace std;
 
 int main()
 {
     int n_int = INT_MAX;
     short n_short = SHRT_MAX;
     long n_long = LONG_MAX;
-    long long n_llong = LONG_LONG_MAX;
+    long long n_llong = LLONG_MAX;
 
+    // sizeof operator yields the size of type or of variable
+    cout << "int is " << sizeof (int) << " bytes" << endl;
+    cout << "short is " << sizeof n_short << " bytes" << endl;
+    cout << "long is " << sizeof n_long << " bytes" << endl;
+    cout << "long long is " << sizeof n_llong << " bytes" << endl;
+
+    cout << "Maximum values:" << endl;
+    cout << "int " << n_int << endl;
+    cout << "short " << n_short << endl;
+    cout << "long " << n_long << endl;
+    cout << "llong " << n_llong << endl;
     
 }
