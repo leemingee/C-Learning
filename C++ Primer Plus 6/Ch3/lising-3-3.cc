7ef53ae28ea6f154ba24d5120d0e10fa02e7b1d5
Ming Li <leemingee1995@gmail.com>
2019-03-06 23:11:34 -0500

    Update lising-3-3.cc

--- C++ Primer Plus 6/Ch3/lising-3-3.cc
+++ C++ Primer Plus 6/Ch3/lising-3-3.cc
@@ -16,6 +16,11 @@ int main()
     int waist = 0x42;
     int inseam = 042;
 
-    
+    cout << "chest " << chest << " (42 in decimal)" << endl;
+    cout << "waist " << waist << " (0x42 in hex)" << endl; // 4 * 16 + 2 = 66
+    cout << "inseam " << inseam << " (042 in octal" << endl; // 4 * 8 + 2 = 34
+    return 0;
 }
 
+// start with 0, and range from 0-7 means the hex
+// start with 0x, and range from 0-9 a-f means the octal
